7ac96b28b4b3c952f10660712c107d7a77f03124
Gabriel Poca <mail@gabrielpoca.com>
2021-04-02 23:16:17 +0100

    LICENSE and hex config

--- mix.exs
+++ mix.exs
@@ -4,8 +4,10 @@ defmodule StillNode.MixProject do
   def project do
     [
       app: :still_node,
+      description: "An Elixir library to manage long-running NodeJS processes",
       version: "0.1.0",
       elixir: "~> 1.11",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,6 +26,21 @@ defmodule StillNode.MixProject do
       {:jason, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/still-ex/still_node"},
+      files: [
+        "LICENSE",
+        "mix*",
+        "lib/*",
+        "priv/*",
+        "assets/package*",
+        "assets/index.js"
+      ]
     ]
   end
 end
